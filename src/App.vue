cd29c41dbaeecd4f512e0f5bbd4299586be9d84d
iamahmedshahh <artistahmed41@gmail.com>
2023-04-05 19:01:32 -0400

    global component practice

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup>
 
-import {RouterView} from "vue-router"
-
-</script>
 <template>
    <main>
     <RouterView/>
+    <DashboardWalletInfo>
+    </DashboardWalletInfo>
+    <AppStrategy></AppStrategy>
+    <AppExport></AppExport>
+    <RecentSwaps></RecentSwaps>
    </main>
 </template>
+<script>
+
+import {RouterView} from "vue-router"
+import DashboardWalletInfo from "./views/DashboardWalletInfo.vue"
+import AppStrategy from "./views/AppStrategy.vue"
+import AppExport from "./views/AppExport.vue"
+import RecentSwaps from './views/RecentSwaps.vue'
+
+export default {
+  components: { 
+   DashboardWalletInfo,
+   AppStrategy,
+   AppExport,
+   RecentSwaps
+ }
+}
+</script>
