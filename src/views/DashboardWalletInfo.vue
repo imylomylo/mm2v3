bdb786d98cc77aa503186580825bd12983953d75
iamahmedshahh <artistahmed41@gmail.com>
2023-06-07 02:56:26 +0500

    width

--- src/views/DashboardWalletInfo.vue
+++ src/views/DashboardWalletInfo.vue
@@ -20,7 +20,7 @@
       </v-chip>
     </v-toolbar>
     <v-divider class="mx-4"></v-divider>
-    <v-table fixed-header height="100%" style="width: 100%;">
+    <v-table fixed-header height="100%">
       <thead>
         <tr>
           <th>TICKER</th>
