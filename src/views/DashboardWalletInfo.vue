acaedd72e8756851875cd865b1815f18b7e4aea8
iamahmedshahh <artistahmed41@gmail.com>
2023-06-07 03:00:58 +0500

    horizontal scroll

--- src/views/DashboardWalletInfo.vue
+++ src/views/DashboardWalletInfo.vue
@@ -20,7 +20,7 @@
       </v-chip>
     </v-toolbar>
     <v-divider class="mx-4"></v-divider>
-    <v-table fixed-header height="100%" style="scroll-behavior: auto;">
+    <v-table fixed-header height="100%" style="max-width: 70%;">
       <thead>
         <tr>
           <th>TICKER</th>
