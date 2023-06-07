94c97c0b53028ebdd65476e5c4b9de040e231ff9
iamahmedshahh <artistahmed41@gmail.com>
2023-06-08 02:27:44 +0500

    hardcoded unresolved

--- src/views/AppMarkets.vue
+++ src/views/AppMarkets.vue
@@ -330,11 +330,11 @@ export default {
       //     rel: rel
       //   }
       // });
-       window.location.href='/traderview?base='+base+'&rel='+rel
+      //window.location.href='/traderview?base='+base+'&rel='+rel
       //window.location.href='/traderview/'+base+'/'+rel
-      ///this.$router.push({name: "TraderView", params: {base: base, rel: rel}})
+      this.$router.push({name: "TraderView", params: {base: base, rel: rel}})
       //this.$forceUpdate()
-      //this.$router.go(this.$router.currentRoute)
+      this.$router.go(this.$router.currentRoute)
 
       // this.forceRerender();
       // "http://" +
