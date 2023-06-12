75fe03b6398163e6587eec51f5b98fa1e6f58d5a
iamahmedshahh <artistahmed41@gmail.com>
2023-06-13 04:01:09 +0500

    qr code aligned center #34

--- src/views/traderview/WalletInfo.vue
+++ src/views/traderview/WalletInfo.vue
@@ -57,8 +57,8 @@
       {{ depositTicker }}: {{ depositAddress }}
       <div style="align-items: center; margin: auto;">
       <qrcode-vue :value="depositAddress" :size="depositOverlaySize" level="L" style="align-self: center;"></qrcode-vue>
-      <v-btn color="success" @click="hideDepositOverlay(false)">Dismiss</v-btn>
       </div>
+      <v-btn color="success" @click="hideDepositOverlay(false)">Dismiss</v-btn>
     </v-overlay>
     <v-overlay opacity="0.88" :absolute="false" :model-value="withdrawOverlay" z-index="6" contained persistent class="align-center justify-center">
       <v-card class="mx-auto" min-width="400">
