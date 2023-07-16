fe0920f21c227bd0f07c176f0710751c27b1e797
iamahmedshahh <artistahmed41@gmail.com>
2023-07-16 05:27:10 +0500

    adding pause play to icon in #76

--- src/views/traderview/AutomatedMarketMaking.vue
+++ src/views/traderview/AutomatedMarketMaking.vue
@@ -37,7 +37,7 @@
           </v-col>
           <v-col class="text-right">
             <v-btn :color="mmcolor" dark depressed fab @click="toggle">
-              <v-icon large>{{ isPlaying ? 'pause' : 'play_arrow' }}</v-icon>
+              <v-icon large>{{ isPlaying ? 'pause' : 'play' }}</v-icon>
             </v-btn>
           </v-col>
         </v-row>
