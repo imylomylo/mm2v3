55f0ba436fed5d07b9226122ffedd1010e2e8b58
iamahmedshahh <artistahmed41@gmail.com>
2023-07-16 05:02:44 +0500

    minor tweaks for #77

--- src/views/traderview/AppTraderView.vue
+++ src/views/traderview/AppTraderView.vue
@@ -7,7 +7,7 @@
                     <div>
                         <v-row no-gutters>
                             <v-toolbar color="white" flat>
-                                <v-toolbar-title style="width:fit-content;">Trading</v-toolbar-title>
+                                <v-toolbar-title style="width:fit-content; margin-right: 10px;">Trading</v-toolbar-title>
                                 <v-divider vertical top-spaced></v-divider>
                                 <h2 style="margin-left: 10px;">{{ wallets.base.ticker + " / " + wallets.rel.ticker}}</h2>
                                 <v-chip class="ma-2" color="success" outlined @click="invertbase(wallets.base.ticker, wallets.rel.ticker)">
