993fd02a81d38e4d98b292cc51505121c334cf83
iamahmedshahh <artistahmed41@gmail.com>
2023-07-19 03:21:33 +0500

    #33 testing marketselection

--- src/App.vue
+++ src/App.vue
@@ -69,8 +69,8 @@ export default {
       this.dialog = !this.dialog
       
       //this.$router.push("/traderview/RICK/MORTY");
-      this.$router.push("/traderview?base=" + base + "&rel=" +rel); 
-      //window.location.href='/traderview/'+base+'/'+rel;
+      //this.$router.push("/traderview?base=" + base + "&rel=" +rel); 
+      window.location.href='/traderview/'+base+'/'+rel;
     },
     doAction: function(command) {
       window.location.href = "/" + command.toLowerCase().replace(/ /g, "");
