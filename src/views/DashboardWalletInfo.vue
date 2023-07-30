d4e8ba0f0ccc00e67e7ac23be66f8a63c205f8a0
iamahmedshahh <artistahmed41@gmail.com>
2023-07-30 05:42:14 +0500

    #29 unfixed

--- src/views/DashboardWalletInfo.vue
+++ src/views/DashboardWalletInfo.vue
@@ -20,7 +20,7 @@
       </v-chip>
     </v-toolbar>
     <v-divider class="mx-4"></v-divider>
-    <v-table fixed-header height="auto" bordered style="position: absolute;">
+    <v-table fixed-header height="auto" bordered>
       <thead>
         <tr>
           <th>TICKER</th>
