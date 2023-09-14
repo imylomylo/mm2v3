770c56b3f041e0d1b67453e494fefb32e348222d
iamahmedshahh <artistahmed41@gmail.com>
2023-09-15 02:59:56 +0500

    singleorder

--- src/views/traderview/SingleOrder.vue
+++ src/views/traderview/SingleOrder.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-card class="mx-auto" outlined>
-      <v-toolbar flat dense>
+      <v-toolbar flat dense color="red lighten-1">
         <v-toolbar-title>
           <span class="subheading">Single Order {{wallets.base.ticker}}</span>
         </v-toolbar-title>
