3cc6e88231373f723ab273fb9285f6981f433898
iamahmedshahh <artistahmed41@gmail.com>
2023-09-20 03:20:55 +0500

    Adding Verus tabs and making components

--- src/views/AppVerus.vue
+++ src/views/AppVerus.vue
@@ -1,24 +1,40 @@
 <template>
     <v-app>
-    <v-card style="margin-top: 100px;">
-        <v-tabs
-        fixed-tabs
-        >
-        <v-tab>
-        Verus Staking
-        </v-tab>
-         <v-tab>
-         Verus ID's
-        </v-tab>
-        <v-tab>
-         Verus Currencies
-        </v-tab>
+      <v-card style="margin-top: 100px;">
+        <v-tabs v-model="activeTab" fixed-tabs>
+          <v-tab value="verus-staking">Verus Staking</v-tab>
+          <v-tab value="verus-ids">Verus ID's</v-tab>
+          <v-tab value="verus-currencies">Verus Currencies</v-tab>
         </v-tabs>
-  </v-card>
+        
+        <v-tab-item v-if="activeTab === 'verus-staking'">
+          <VerusStaking/>
+        </v-tab-item>
+        
+        <v-tab-item v-if="activeTab === 'verus-ids'">
+          <VerusId/>
+        </v-tab-item>
+        
+        <v-tab-item v-if="activeTab === 'verus-currencies'">
+          <VerusCurrencies/>
+        </v-tab-item>
+      </v-card>
     </v-app>
-</template>
-<script>
-export default{
+  </template>
+  
+  <script>
+  import VerusCurrencies from './verus/VerusCurrencies.vue';
+  import VerusStaking from './verus/VerusStaking.vue';
+  import VerusId from './verus/VerusId.vue';
+  
+  export default {
     name: 'Verus',
-}
-</script>
+    components: { VerusCurrencies, VerusStaking, VerusId },
+    data() {
+      return {
+        activeTab: 'verus-staking', // Set the default active tab here
+      };
+    },
+  };
+  </script>
+  
