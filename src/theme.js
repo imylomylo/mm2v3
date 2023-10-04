1f8be751e369118d5c3aadfd17bd295b53ecd046
iamahmedshahh <artistahmed41@gmail.com>
2023-10-05 02:41:58 +0500

    testing themes

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,28 @@
+// theme.js
+
+const themes = {
+  komodo: {
+    primary: '#4CAF50', // Green for Komodo
+    secondary: '#424242',
+    accent: '#82B1FF',
+  },
+  verus: {
+    primary: '#1976D2', // Blue for Verus
+    secondary: '#424242',
+    accent: '#82B1FF',
+  },
+  zcash: {
+    primary: '#FFD700', // Golden for Zcash
+    secondary: '#FFFFFF', // White
+    accent: '#82B1FF',
+  },
+};
+
 export default {
-    // Define your color palette
-    theme: {
-      dark: false, // Set to true for a dark theme
-      themes: {
-        light: {
-          primary: '#1976D2', // Your primary color
-          secondary: '#424242', // Your secondary color
-          accent: '#82B1FF', // Your accent color
-        },
-        dark: {
-          primary: '#2196F3',
-          secondary: '#616161',
-          accent: '#FFC107',
-        },
-      },
-    },
-  };
-  
+  // Define your color palette
+  theme: {
+    dark: false, // Set to true for a dark theme
+    themes: themes.komodo, // Default theme (Komodo)
+  },
+  themes, // Export all themes for dynamic switching
+};
