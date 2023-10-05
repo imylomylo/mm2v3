9f547b612f02d6060a25665afd79fafda0e43b72
iamahmedshahh <artistahmed41@gmail.com>
2023-10-06 02:20:59 +0500

    updated theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,28 +1,70 @@
-// theme.js
-
 const themes = {
   komodo: {
     primary: '#4CAF50', // Green for Komodo
     secondary: '#424242',
     accent: '#82B1FF',
+    button: {
+      primary: '#4CAF50', // Green for Komodo
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
+    appbar: {
+      primary: '#4CAF50', // Green for Komodo
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
+    toolbar: {
+      primary: '#4CAF50', // Green for Komodo
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
   },
   verus: {
     primary: '#1976D2', // Blue for Verus
     secondary: '#424242',
     accent: '#82B1FF',
+    button: {
+      primary: '#1976D2', // Blue for Verus
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
+    appbar: {
+      primary: '#1976D2', // Blue for Verus
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
+    toolbar: {
+      primary: '#1976D2', // Blue for Verus
+      secondary: '#424242',
+      accent: '#82B1FF',
+    },
   },
   zcash: {
     primary: '#FFD700', // Golden for Zcash
     secondary: '#FFFFFF', // White
     accent: '#82B1FF',
+    button: {
+      primary: '#FFD700', // Golden for Zcash
+      secondary: '#FFFFFF', // White
+      accent: '#82B1FF',
+    },
+    appbar: {
+      primary: '#FFD700', // Golden for Zcash
+      secondary: '#FFFFFF', // White
+      accent: '#82B1FF',
+    },
+    toolbar: {
+      primary: '#FFD700', // Golden for Zcash
+      secondary: '#FFFFFF', // White
+      accent: '#82B1FF',
+    },
   },
 };
 
 export default {
-  // Define your color palette
   theme: {
-    dark: false, // Set to true for a dark theme
-    themes: themes.komodo, // Default theme (Komodo)
+    dark: false,
+    themes,
   },
-  themes, // Export all themes for dynamic switching
+  themes,
 };
