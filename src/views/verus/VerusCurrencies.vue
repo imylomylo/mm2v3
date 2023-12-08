e8640fe8cdc5fb0a57735c0a8f135fd29b26a25a
iamahmedshahh <artistahmed41@gmail.com>
2023-12-09 01:10:34 +0500

    addded request

--- src/views/verus/VerusCurrencies.vue
+++ src/views/verus/VerusCurrencies.vue
@@ -7,14 +7,17 @@
   </template>
 <script>
 
-import {VerusdRpcInterface} from './verusd-rpc-ts-client/src/index'
+import {ApiRequest} from './verus-typescript-primitives/src/api/ApiRequest.ts'
+import {GetInfoRequest} from './verus-typescript-primitives/src/api/classes/GetInfo/GetInfoRequest.ts'
+
 
 export default{
     name: 'VerusCurrencies',
     mounted() {
-        
-    }
+    const infoRequest = new GetInfoRequest('VRSC');
 
+    // Log the result of the toJson() method to the console
+    console.log(infoRequest.toJson());
 }
-
+}
 </script>
